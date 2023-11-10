8b6ec6422f7b725518e8330d8a41cf879068c97e
happycastle <41810556+happycastle114@users.noreply.github.com>
2023-11-11 00:36:08 +0900

    Add: MongoDB

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from dotenv import load_dotenv
 from utils import getPRs, getCommits
-from model import PR, Commit
+from model import PR, Commit, client
 from typing import List, Dict, Union, Optional
+from contextlib import asynccontextmanager
+import os
+
+
 load_dotenv()
 
-app = FastAPI()
+@asynccontextmanager
+async def initMongo(app: FastAPI):
+    client.connect()
+    yield
+    client.close()
+
+app = FastAPI(lifespan=initMongo)
+
 
 @app.get("/pull/{owner}/{repo}", response_model=List[PR])
 async def getpull(owner: str = ..., repo: str = ...):
--- backend/model/__init__.py
+++ backend/model/__init__.py
@@ -1,1 +1,2 @@
 from .PR import PR, Commit
+from .MongoDB import client
