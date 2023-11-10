27e261316dfa1fea6ddd6371892f1c08c2254ae9
star <snaoyam@gmail.com>
2023-11-11 05:35:41 +0900

    merge station

--- backend/station2.py
+++ backend/station2.py
@@ -1,11 +1,13 @@
 import math
 import random
 import matplotlib.pyplot as plt
-from time import sleep
 
-seed = "1A13222212K"
-jump_generate = 20
+seed = "1A132212K"
+init_location_sigma = 10
 jump_path = 15
+personcnt = 3
+order = [0,0,2,0,0,2,1,1,1,0,1,2,2,2,2,2,1,0,0,1,1,1,1,0,0,0,1,1,0,0,1, 0,0,2,0,0,2,1,1]
+merge_criteria = 5
 
 def polar_to_cartesian(r, theta):
     x = r * math.cos(math.pi * theta / 180)
@@ -26,34 +28,48 @@ center = (width/2, height/2)
 radius = (width ** 2 + height ** 2) ** 0.5 / 2
 
 random.seed(seed)
-personcnt = 3
-order = [0,0,2,0,0,2,1,1,1,0,1,2,2,2,2,2,1,0,0,1,1,1,1,0,0,0,1,1,0,0,1]
-path = [([(0, 0)], random.uniform(0, math.pi)) for i in range(personcnt)]
+path = [([(random.uniform(-init_location_sigma, init_location_sigma), random.uniform(-init_location_sigma, init_location_sigma))], random.uniform(0, math.pi)) for i in range(personcnt)]
+all_points = []
+for pa in path:
+    for p in pa[0]:
+        all_points.append(p)
 
-def next_point(src_point, slope, distance):
-    return (src_point[0] + distance * math.cos(slope), src_point[1] + distance * math.sin(slope))
+def find_any_close_point(point, criteria, cluster):
+    for other_point in cluster:
+        distance = ((point[0] - other_point[0]) ** 2 + (point[1] - other_point[1]) ** 2) ** 0.5
+        if(distance < criteria):
+            return other_point
+    else:
+        return point
+
+def next_point(src_point, slope, distance, all_points = []):
+    next = (src_point[0] + distance * math.cos(slope), src_point[1] + distance * math.sin(slope))
+    return find_any_close_point(next, merge_criteria, all_points)
 
 def get_next_point(personindex, path):
     path_comp = path[personindex][0]
     slope = path[personindex][1]
     end_point_distance_pos = path_comp[0][0] ** 2 + path_comp[0][1] ** 2
     end_point_distance_neg = path_comp[-1][0] ** 2 + path_comp[-1][1] ** 2
+
     #pos extend
     if (end_point_distance_pos < end_point_distance_neg):
         if (len(path_comp) > 1):
             slope = math.atan2(path_comp[0][1] - path_comp[1][1], path_comp[0][0] - path_comp[1][0])
-            new_point = next_point(path_comp[0], slope + random.uniform(-math.pi/12, math.pi/12), jump_path + random.uniform(-jump_path/3, jump_path/3))
+            new_point = next_point(path_comp[0], slope + random.uniform(-math.pi/10, math.pi/10), jump_path + random.uniform(-jump_path/3, jump_path/3), all_points)
         else:
             new_point = (path_comp[0][0] + jump_path * math.cos(slope), path_comp[0][0] + jump_path * math.sin(slope))
         path_comp.insert(0, new_point)
+        all_points.append(new_point)
     #neg extend
     else:
         if (len(path_comp) > 1):
             slope = math.atan2(path_comp[-1][1] - path_comp[-2][1], path_comp[-1][0] - path_comp[-2][0])
-            new_point = next_point(path_comp[-1], slope + random.uniform(-math.pi/12, math.pi/12), jump_path + random.uniform(-jump_path/3, jump_path/3))
+            new_point = next_point(path_comp[-1], slope + random.uniform(-math.pi/10, math.pi/10), jump_path + random.uniform(-jump_path/3, jump_path/3), all_points)
         else:
             new_point = (path_comp[0][0] - jump_path * math.cos(slope), path_comp[0][0] - jump_path * math.sin(slope))
         path_comp.append(new_point)
+        all_points.append(new_point)
 
     path[personindex] = (path_comp, slope)
     return path
